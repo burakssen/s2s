4088a11aedd7f538a7c508c05959148e051c0e27
tranzystorekk <tranzystorek.io@protonmail.com>
2025-08-31 21:43:15 +0200

    chore: update build script for Zig 0.15

--- build.zig
+++ build.zig
@@ -4,14 +4,14 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    _ = b.addModule("s2s", .{
-        .root_source_file = b.path("s2s.zig"),
-    });
-
-    const tests = b.addTest(.{
+    const s2s_module = b.addModule("s2s", .{
         .root_source_file = b.path("s2s.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const tests = b.addTest(.{
+        .root_module = s2s_module,
     });
 
     const test_step = b.step("test", "Run unit tests");
